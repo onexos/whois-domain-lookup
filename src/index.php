cf78452cb9fc0439fd018c645f6a8c0f854d2e41
onexos <onexroad@gmail.com>
2026-02-22 00:23:51 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -228,7 +228,7 @@ if ($_SERVER["QUERY_STRING"] ?? "") {
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght,SOFT,WONK@72,600,50,1&display=swap">
-  
+  // <?= CUSTOM_HEAD ?>
 </head>
 
 <body>
